e4c2f19dc2560bd4952b97fbab43eb43835f70fa
tifnguyen03 <tifnguyen03@gmail.com>
2023-03-26 16:24:58 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// Smooth scrolling
+$('a[href*="#"]').on('click', function(e) {
+    e.preventDefault();
+  
+    $('html, body').animate(
+      {
+        scrollTop: $($(this).attr('href')).offset().top,
+      },
+      500,
+      'linear'
+    );
+  });
+  
